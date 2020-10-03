6d85e993b88ae357d11b7192e7f380b9602da977
chenhui <chenhui@2043600393.com>
2020-10-03 13:55:04 +0800

    初始导入01

---
+++ src/com/chenhui/domain/Person.java
@@ -0,0 +1,12 @@
+package com.chenhui.domain;
+
+/**
+ * ClassName : Person
+ * Package : com.chenhui.domain
+ * Description :
+ *
+ * @author : chenhui@xvco.com
+ */
+public class Person {
+    private String name;
+}
